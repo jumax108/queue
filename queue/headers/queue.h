63dc8cd837bb0b2b20d288f99ff6f2aa455fab20
unknown <jumax108@kakao.com>
2022-03-11 17:18:22 +0900

    push, pop 할 때 함수 호출 위치 기록하는 기능 추가

--- queue/headers/queue.h
+++ queue/headers/queue.h
@@ -2,8 +2,20 @@
 
 #include <Windows.h>
 
+#include "common.h"
+
+#if defined(QUEUE_LOG)
+	#define push(data) _push(data, __FILEW__, __LINE__)
+	#define pop() _pop(__FILEW__, __LINE__)
+#else 
+	#define push(data) _push(data)
+	#define pop() _pop()
+#endif
+
 template<typename T>
 class CQueue {
+
+	struct stNode;
 
 public:
 
@@ -17,13 +29,21 @@ public:
 		return _capacity;
 	}
 
-	bool push(T in);
-	bool pop();
+	bool _push(T in
+		#if defined(QUEUE_LOG)
+			,const wchar_t* file, int line
+		#endif
+	);
+	bool _pop(
+		#if defined(QUEUE_LOG)
+			const wchar_t* file, int line
+		#endif
+	);
 	bool front(T* out);
 
 private:
 
-	T* _data;
+	stNode* _data;
 	unsigned int _size;
 	unsigned int _capacity;
 	unsigned int _rear;
@@ -33,13 +53,45 @@ private:
 	void resize(unsigned int capapcity);
 
 	HANDLE _heap;
+
+		struct stNode{
+
+	public:
+		inline stNode(T& data){
+			_data = data;
+			#if defined(QUEUE_LOG)
+				_pushFile = nullptr;
+				_pushLine = 0;
+				_popFile  = nullptr;
+				_popFile  = 0;
+			#endif
+		}
+		#if defined(QUEUE_LOG)
+			inline void pushLog(const wchar_t* file, int line){
+				_pushFile = (wchar_t*)file;
+				_pushLine = line;
+			}
+			inline void popLog(const wchar_t* file, int line){
+				_popFile = (wchar_t*)file;
+				_popLine = line;
+			}
+		#endif
+	private:
+		T _data;
+		#if defined(QUEUE_LOG)
+			wchar_t* _pushFile;
+			int _pushLine;
+			wchar_t* _popFile;
+			int _popLine;
+		#endif
+	};
 };
 
 template<typename T>
 CQueue<T>::CQueue(unsigned int capacity, unsigned int maxCapacity) {
 
 	_heap = HeapCreate(0,0,0);
-	_data = (T*)HeapAlloc(_heap, HEAP_ZERO_MEMORY, sizeof(T) * capacity);
+	_data = (stNode*)HeapAlloc(_heap, HEAP_ZERO_MEMORY, sizeof(stNode) * capacity);
 
 	_capacity = capacity;
 	_maxCapacity = maxCapacity;
@@ -60,10 +112,10 @@ CQueue<T>::~CQueue() {
 template <typename T>
 void CQueue<T>::resize(unsigned int capacity){
 
-	T* oldData = _data;
-	_data = (T*)HeapAlloc(_heap, 0, sizeof(T) * capacity);
+	stNode* oldData = _data;
+	_data = (stNode*)HeapAlloc(_heap, 0, sizeof(stNode) * capacity);
 
-	memcpy(_data, oldData, sizeof(T) * _capacity);
+	memcpy(_data, oldData, sizeof(stNode) * _capacity);
 	
 	_capacity = capacity;
 	HeapFree(_heap, 0, oldData);
@@ -71,7 +123,11 @@ void CQueue<T>::resize(unsigned int capacity){
 }
 
 template<typename T>
-bool CQueue<T>::push(T in) {
+bool CQueue<T>::_push(T in
+	#if defined(QUEUE_LOG)
+		,const wchar_t* file, int line
+	#endif
+) {
 
 	if (_size == _capacity) {
 		if(_capacity == _maxCapacity){
@@ -85,7 +141,13 @@ bool CQueue<T>::push(T in) {
 		resize(nextCapacity);
 	}
 
-	_data[_rear] = in;
+	stNode* node = &_data[_rear];
+	new (node) stNode(in);
+
+	#if defined(QUEUE_LOG)
+		node->pushLog(file, line);
+	#endif
+
 	_rear = (_rear + 1) % _capacity;
 	_size += 1;
 
@@ -94,11 +156,19 @@ bool CQueue<T>::push(T in) {
 }
 
 template<typename T>
-bool CQueue<T>::pop() {
+bool CQueue<T>::_pop(
+	#if defined(QUEUE_LOG)
+		const wchar_t* file, int line
+	#endif
+) {
 
 	if (_size == 0) {
 		return false;
 	}
+
+	#if defined(QUEUE_LOG)
+		_data[_front].popLog(file, line);
+	#endif
 
 	_front = (_front + 1) % _capacity;
 	_size -= 1;
